66274322ebd44cf0ec2fcd0faa63274fb95bdd1b
MiguelAngelCastro7879 <mikeangel7879@gmail.com>
2021-09-28 16:42:41 -0500

    Se declaran los botones usando variables en el MainActivity y se buscan por su Id

--- app/src/main/java/com/example/proyectosndroid/MainActivity.java
+++ app/src/main/java/com/example/proyectosndroid/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.proyectosndroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
     }
 }
