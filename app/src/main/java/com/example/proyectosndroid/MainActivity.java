91e7beabbf024ffd68caa25a889c04d952fcc8b4
MiguelAngelCastro7879 <mikeangel7879@gmail.com>
2021-09-28 18:04:24 -0500

    Se crea un codigo para poder abrir los otros activity desde el ActivityMain, se comenta el redireccionamiento de los botones 2-9 para evitar errores momentaneos

--- app/src/main/java/com/example/proyectosndroid/MainActivity.java
+++ app/src/main/java/com/example/proyectosndroid/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
@@ -13,18 +15,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);
-        btn5 = findViewById(R.id.btn5);
-        btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
 
     }
 
-    public void ir_a(View view) {
+    public void ir_a(View v) {
+
+        btn1 = findViewById(R.id.btn1);btn2 = findViewById(R.id.btn2);btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);btn5 = findViewById(R.id.btn5);btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);btn8 = findViewById(R.id.btn8);btn9 = findViewById(R.id.btn9);
+
+        Button b=(Button) v;
+        switch (b.getId()){
+            case R.id.btn1:
+                setContentView(R.layout.linear_activity);
+                break;
+//            case R.id.btn2:
+//                setContentView(R.layout.linear_activity);
+//                break;
+//            case R.id.btn3:
+//                setContentView(R.layout.linear_activity);
+//                break;
+//            case R.id.btn4:
+//                setContentView(R.layout.linear_activity);
+//                break;
+//            case R.id.btn5:
+//                setContentView(R.layout.linear_activity);
+//                break;
+//            case R.id.btn6:
+//                setContentView(R.layout.linear_activity);
+//                break;
+//            case R.id.btn7:
+//                setContentView(R.layout.linear_activity);
+//                break;
+//            case R.id.btn8:
+//                setContentView(R.layout.linear_activity);
+//                break;
+//            case R.id.btn9:
+//                setContentView(R.layout.linear_activity);
+//                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + b.getId());
+        }
+
     }
 }
