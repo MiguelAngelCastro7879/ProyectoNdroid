c0bd5c0afaf3d1ea9d61e9a56cbb541a381d3e78
MiguelAngelCastro7879 <mikeangel7879@gmail.com>
2021-10-06 12:18:54 -0500

    Se termina de crear el diseño para la calculadora usando constraint layout y lineas guias

--- app/src/main/java/com/example/proyectosndroid/MainActivity.java
+++ app/src/main/java/com/example/proyectosndroid/MainActivity.java
@@ -35,18 +35,17 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn3:
                 setContentView(R.layout.linear_activity2);
                 break;
-//            case R.id.btn4:
-//                setContentView(R.layout.constraint_activity);
-//                break;
-//            case R.id.btn5:
-//                setContentView(R.layout.linear_activity);
-//                break;
-            case R.id.btn6:
+            case R.id.btn4:
+                setContentView(R.layout.linear_relative_activity);
+                break;
+            case R.id.btn5:
                 setContentView(R.layout.constraint_activity);
                 break;
-            case R.id.btn7:
+            case R.id.btn6:
                 setContentView(R.layout.constraint_activity2);
                 break;
+//            case R.id.btn7:
+//                break;
 //            case R.id.btn8:
 //                setContentView(R.layout.linear_activity);
 //                break;
