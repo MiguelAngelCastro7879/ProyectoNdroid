ab03841937d65dd8308d26a7aa5275fe82d86482
MiguelAngelCastro7879 <mikeangel7879@gmail.com>
2021-10-16 15:26:57 -0500

    Se modifican todos los xml para encapsular las strings para evitar advertencias, en el MainActivity se realizan las acciones SetOnClickListener para todos los botones del activity_main.xml para poder cambiar el contenido de la view por diferentes views

--- app/src/main/java/com/example/proyectosndroid/MainActivity.java
+++ app/src/main/java/com/example/proyectosndroid/MainActivity.java
@@ -8,25 +8,32 @@ import android.widget.Toast;
 
 import java.io.BufferedReader;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    //Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
+        findViewById(R.id.btn3).setOnClickListener(this);
+        findViewById(R.id.btn4).setOnClickListener(this);
+        findViewById(R.id.btn5).setOnClickListener(this);
+        findViewById(R.id.btn6).setOnClickListener(this);
+        findViewById(R.id.btn8).setOnClickListener(this);
+        findViewById(R.id.btn9).setOnClickListener(this);
 
     }
 
-    public void ir_a(View v) {
+    public void texto(View view) {
+        Toast.makeText(this, "Holaaaaa", Toast.LENGTH_LONG).show();
+    }
 
-        btn1 = findViewById(R.id.btn1);btn2 = findViewById(R.id.btn2);btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);btn5 = findViewById(R.id.btn5);btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);btn8 = findViewById(R.id.btn8);btn9 = findViewById(R.id.btn9);
-
-        Button b=(Button) v;
-        switch (b.getId()){
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
             case R.id.btn1:
                 setContentView(R.layout.linear_activity);
                 break;
@@ -45,18 +52,14 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn6:
                 setContentView(R.layout.constraint_activity2);
                 break;
-//            case R.id.btn8:
-//                break;
+            case R.id.btn8:
+                setContentView(R.layout.tictactoe);
+                break;
 //            case R.id.btn9:
 //                setContentView(R.layout.linear_activity);
 //                break;
             default:
-                throw new IllegalStateException("Unexpected value: " + b.getId());
+                throw new IllegalStateException("Unexpected value: " + v.getId());
         }
-
-    }
-
-    public void texto(View view) {
-        Toast.makeText(this, "Holaaaaa", Toast.LENGTH_LONG).show();
     }
 }
