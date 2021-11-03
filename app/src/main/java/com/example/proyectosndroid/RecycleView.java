a583a4300525192a7dce3f33223b4da00e442a90
MiguelAngelCastro7879 <mikeangel7879@gmail.com>
2021-11-02 23:28:32 -0600

    se crean las variables y metods necesarios para la entidad

--- app/src/main/java/com/example/proyectosndroid/RecycleView.java
+++ app/src/main/java/com/example/proyectosndroid/RecycleView.java
@@ -2,9 +2,12 @@ package com.example.proyectosndroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class RecycleView extends AppCompatActivity {
 
+    private RecyclerView lista;
+    private recycle_adaptador adaptador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
