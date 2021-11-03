c83bcd7e95dac863bc8c768885f64fadcff557d7
MiguelAngelCastro7879 <mikeangel7879@gmail.com>
2021-11-02 23:49:25 -0600

    Se llama el adaptador en el RecycleView para poder llenar el Recycleview de varios items, se crea el ciclo de for para que se llene con 30 items

--- app/src/main/java/com/example/proyectosndroid/RecycleView.java
+++ app/src/main/java/com/example/proyectosndroid/RecycleView.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class RecycleView extends AppCompatActivity {
 
     private RecyclerView lista;
@@ -12,5 +14,17 @@ public class RecycleView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle_view);
+        lista=findViewById(R.id.lista);
+        adaptador=new recycle_adaptador(getArrayItems(),this);
+    }
+
+    private ArrayList<recycle_entidad> getArrayItems() {
+        ArrayList<recycle_entidad> lista = new ArrayList<>();
+        for(int i=0;i<30;i++)
+        {
+            lista.add(new recycle_entidad(R.drawable.ic_launcher_background,"Titulo # "+i,  "subtitulo #"+i));
+
+        }
+        return lista;
     }
 }
